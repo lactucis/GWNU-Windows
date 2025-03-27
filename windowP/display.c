e05ec28c53612c179b0469522866a64dcd3b440e
lettuce <sw701110@gmail.com>
2025-03-27 11:46:35 +0900

    0327

---
+++ windowP/display.c
@@ -0,0 +1,14 @@
+#include "display.h"
+
+int menu() {
+	puts("********************");
+	puts("*   윈도우즈게임   *");
+	puts("********************");
+	puts("");
+	puts("1. 게임시작");
+	puts("2. 종료");
+	puts("");
+	printf("입력>");
+
+	return 0;
+}
---
+++ windowP/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include "display.h"
+
+
+
+int game() {
+	system("cls");
+	puts("게임 시작!");
+	puts("");
+
+
+	int pos = 5;
+	char move;
+
+	while (1) {
+		for (int i = 0; i < 11; i++)
+		{
+			if (i == pos) {
+				printf("0");
+			}
+			else
+			{
+				printf("_");
+			}
+		}
+
+		printf("\n");
+
+		move = _getch();
+
+		if (move == 'a' && pos > 0)
+		{
+			pos--;
+		}
+		else if (move == 'd' && pos < 10) {
+			pos++;
+		}
+
+		system("cls");
+	}
+}
+
+
+int main() {
+	menu();
+
+	char c = getch();
+
+	if (c == '1')
+	{
+		game();
+	}
+	else if(c == '2')
+	{
+		puts("게임이 종료됩니다.");
+	}
+	else
+	{
+		puts("잘못된 입력입니다.");
+	}
+
+
+	return 0;
+}
