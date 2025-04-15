849af00dbef208eec2020dcbeb81e7b3dce507d0
lettuce <sw701110@gmail.com>
2025-04-15 17:57:21 +0900

    7주차 화

---
+++ W05/7_1.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+
+	while (1) {
+		printf("-(-_- )-");
+		Sleep(500);
+		system("cls");
+		printf("-( -_-)-");
+		Sleep(500);
+		system("cls");
+		}
+
+	return 0;
+}
